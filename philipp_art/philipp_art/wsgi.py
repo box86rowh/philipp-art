7303a9bcc2479dca1a7d5d5cc52679000f287977
box86rowh <jasonatsmtc@hotmail.com>
2015-02-02 13:09:46 -0500

    apache...

--- philipp_art/philipp_art/wsgi.py
+++ philipp_art/philipp_art/wsgi.py
@@ -8,7 +8,21 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import site
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/root/.virtualenvs/philipp-art/local/lib/python2.7/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/ec2-user/django-projects/philipp-art')
+sys.path.append('/home/ec2-user/django-projects/philipp-art/philipp_art')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "philipp_art.settings.amazon")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# Activate your virtual env
+activate_env=os.path.expanduser("root/.virtualenvs/philipp-art/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
