4ea6ad249e96a8216c484a5114461fc1d14aef06
box86rowh <jasonatsmtc@hotmail.com>
2015-02-02 17:23:44 -0500

    logging

--- philipp_art/philipp_art/settings/amazon.py
+++ philipp_art/philipp_art/settings/amazon.py
@@ -112,3 +112,36 @@ STATICFILES_DIRS = (
 )
 
 LOGGING_CONFIG = None
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+        },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+            },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+            },
+        },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+            },
+        'cities': {
+            'handlers': ['console'],
+            'level': 'INFO'
+            },
+
+    }
+}
